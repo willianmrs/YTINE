b48a0a0a09ca1238448d335fa2d748ce42af5bfc
Willian Moreira <willianmrs@gmail.com>
2015-10-01 12:12:37 -0300

    blabla

--- semantica.py
+++ semantica.py
@@ -1,6 +1,8 @@
 from parser import *
 
+
 class Semantica():
+
     def __init__(self, code):
         self.table = {}
         self.scope = "global"
@@ -15,80 +17,81 @@ class Semantica():
             self.inst_composta(self.tree.children[0])
 
     def dec_procedimento(self, no):
-        proc=no.children[0]
+        proc = no.children[0]
         self.scope = proc.leaf[0]
         qtd_param = self.list_param_real(proc.children[0])
 
         if proc.leaf[0] in self.table.keys():
-            print("Erro Semantico. Procedimento '"+proc.leaf[0]+ "' já declarado")
+            print(
+                "Erro Semantico. Procedimento '"+proc.leaf[0] + "' já declarado")
             exit(1)
 
         self.table[proc.leaf[0]] = ['procedimento', qtd_param]
 
         self.inst_composta(proc.children[1])
 
-        self.scope='global';
+        self.scope = 'global'
 
-        if len(no.children)>1:
+        if len(no.children) > 1:
             self.dec_procedimento(no.children[1])
 
     def list_param_real(self, no):
-        tipos=[]
-        if len(no.children)>0:
+        tipos = []
+        if len(no.children) > 0:
             tipo = self.get_tipo(no.children[0])
             tipos.append(tipo)
             self.table[str(self.scope+"."+no.leaf[0])] = ['var', 0, tipo]
-            if len(no.children)>1:
-                tipos=tipos+self.list_param_real(no.children[1])
+            if len(no.children) > 1:
+                tipos = tipos+self.list_param_real(no.children[1])
         return tipos
 
     def inst_composta(self, no):
         self.instrucao(no.children[0])
-        if len(no.children)>1:
+        if len(no.children) > 1:
             self.inst_composta(no.children[1])
-    
+
     def get_tipo(self, no):
         return no.leaf[0]
     ##########Intrução##########
+
     def instrucao(self, no):
-        # declaracao 
+        # declaracao
         if no.children[0].name == 'declaracao_tipo':
             self.declaracao(no.children[0])
 
         # repita
         if no.children[0].name == 'repita':
             self.repita(no.children[0])
-            
+
         # condicional
         if no.children[0].name == 'condicional':
             self.condicional(no.children[0])
-            
-        # atribuicao 
+
+        # atribuicao
         if no.children[0].name == 'atribuicao':
             self.atribuicao(no.children[0])
-            
-        # leitura 
+
+        # leitura
         if no.children[0].name == 'leitura':
             self.leitura(no.children[0])
-            
-        # escrita 
+
+        # escrita
         if no.children[0].name == 'escrita':
             self.expr(no.children[0].children[0])
-            
 
-        # chamada_procedimento 
+        # chamada_procedimento
         if no.children[0].name == 'call_procedimento':
             self.chamada_procedimento(no.children[0])
-            
+
     def declaracao(self, no):
         tipo = self.get_tipo(no.children[0])
         if self.scope+"."+no.leaf[0] in self.table.keys():
-            print("Erro Semantico. Variavel '"+no.leaf[0]+ "' já declarada")
+            print("Erro Semantico. Variavel '"+no.leaf[0] + "' já declarada")
             exit(1)
 
         if no.leaf[0] in self.table.keys():
-            print("Erro Semantico. '"+no.leaf[0]+ "' é um procedimento,"+\
-            " não pode criar variaveis com o mesmo nome do procedimento")
+            print("Erro Semantico. '"+no.leaf[0] + "' é um procedimento," +
+                  " não pode criar variaveis com o mesmo nome do procedimento")
             exit(1)
         self.table[self.scope+"."+no.leaf[0]] = ["var", 0, tipo]
 
@@ -99,36 +102,38 @@ class Semantica():
     def condicional(self, no):
         self.expr(no.children[0])
         self.inst_composta(no.children[1])
-        if len(no.children)==3:
+        if len(no.children) == 3:
             self.inst_composta(no.children[2])
 
     def atribuicao(self, no):
         if self.scope+"."+no.leaf[0] not in self.table.keys():
-            print("Erro Semantico. Variavel '"+no.leaf[0]+ "' não encontrada")
+            print("Erro Semantico. Variavel '"+no.leaf[0] + "' não encontrada")
             exit(1)
         self.expr(no.children[0])
 
     def leitura(self, no):
         if self.scope+"."+no.leaf[0] not in self.table.keys():
-            print("Erro Semantico. Variavel '"+no.leaf[0]+ "' não encontrada")
+            print("Erro Semantico. Variavel '"+no.leaf[0] + "' não encontrada")
             exit(1)
 
     def chamada_procedimento(self, no):
         if no.leaf[0] not in self.table.keys():
-            print("Erro Semantico. Procedimento '"+no.leaf[0]+ "' não declarado")
+            print(
+                "Erro Semantico. Procedimento '"+no.leaf[0] + "' não declarado")
             exit(1)
         param = self.list_param(no.children[0])
         if len(self.table[no.leaf[0]][1]) != param:
-            print("Erro Semantico. Esperado(s) '"+str(len(self.table[no.leaf[0]][1]))+\
-                            "' argumento(s), mais foi(ram) passado(s) '"+str(param)+"'")
+            print("Erro Semantico. Esperado(s) '"+str(len(self.table[no.leaf[0]][1])) +
+                  "' argumento(s), mais foi(ram) passado(s) '"+str(param)+"'")
             exit(1)
-    def list_param(self,no):
-        if len(no.children)>1:
-            return self.list_param(no.children[1])+1;
-        elif len(no.children)==1:
-            return 1;
+
+    def list_param(self, no):
+        if len(no.children) > 1:
+            return self.list_param(no.children[1])+1
+        elif len(no.children) == 1:
+            return 1
         else:
-            return 0;
+            return 0
 
     ############################
 
@@ -137,7 +142,7 @@ class Semantica():
 
     def expr(self, no):
         self.expr_simples(no.children[0])
-        if len(no.children)>1:
+        if len(no.children) > 1:
             op = self.get_op(no.children[1])
             self.expr_simples(no.children[2])
 
@@ -147,9 +152,9 @@ class Semantica():
         else:
             self.expr_simples(no.children[0])
             self.expr_simples(no.children[1])
-    
+
     def termo(self, no):
-        if len(no.children)>1:
+        if len(no.children) > 1:
             self.termo(no.children[0])
             self.fator(no.children[2])
         else:
@@ -158,11 +163,11 @@ class Semantica():
     def fator(self, no):
         if no.name == 'id':
             if self.scope+"."+no.leaf[0] not in self.table.keys():
-                print("Erro Semantico. Variavel '"+no.leaf[0]+ "' não encontrada")
+                print(
+                    "Erro Semantico. Variavel '"+no.leaf[0] + "' não encontrada")
                 exit(1)
         if no.name == 'fator_1':
             self.expr(no.children[0])
-
 
 
 if __name__ == '__main__':
@@ -170,5 +175,4 @@ if __name__ == '__main__':
     code = open(sys.argv[1])
     s = Semantica(code.read())
     s.inicio()
-    print("Tabela de Simbolos:",s.table)
-
+    print("Tabela de Simbolos:", s.table)
