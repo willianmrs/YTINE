af948a50fddf1e388d8004960314ad6e5a564f77
Willian Moreira <willianmrs@gmail.com>
2015-10-01 09:53:52 -0300

    Refatoração de código.

--- lex.py
+++ lex.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-
 import ply.lex as lex
 
 # Lista de Palavras Reservadas
 reservadas = {
-    'se':'SE',
-    'então':'ENTAO',
-    'senão':'SENAO',
-    'fim':'FIM',
-    'repita':'REPITA',
-    'até':'ATE',
-    'leia':'LEIA',
-    'escreve':'ESCREVE',
-    'procedimento':'PROCEDIMENTO',
-    'flutuante':'FLUTUANTE',
-    'inteiro':'INTEIRO',
-    }
+    'se': 'SE',
+    'então': 'ENTAO',
+    'senão': 'SENAO',
+    'fim': 'FIM',
+    'repita': 'REPITA',
+    'até': 'ATE',
+    'leia': 'LEIA',
+    'escreve': 'ESCREVE',
+    'procedimento': 'PROCEDIMENTO',
+    'flutuante': 'FLUTUANTE',
+    'inteiro': 'INTEIRO',
+}
 
 # Lista de Tokens.
 tokens = [
@@ -38,25 +37,29 @@ tokens = [
 
 # Expressões regulares simples.
 
-t_MAIS      = r'\+'
-t_MENOS     = r'-'
-t_VEZES     = r'\*'
-t_DIVIDIR   = r'\/'
-t_IGUAL     = r'\='
-t_VIRGULA   = r'\,'
-t_MENOR     = r'\<'
-t_ABREPARENTES  = r'\('
+t_MAIS = r'\+'
+t_MENOS = r'-'
+t_VEZES = r'\*'
+t_DIVIDIR = r'\/'
+t_IGUAL = r'\='
+t_VIRGULA = r'\,'
+t_MENOR = r'\<'
+t_ABREPARENTES = r'\('
 t_FECHAPARENTES = r'\)'
 t_PONTOEVIRGULA = r';'
-t_RECEBE    = r':='
+t_RECEBE = r':='
 
 # Expressões regulares mais complexas
+
+
 def t_ID(t):
     r'[a-zA-Zà-ú][0-9a-zà-úA-Z]*'
-    t.type = reservadas.get(t.value,'ID')
+    t.type = reservadas.get(t.value, 'ID')
     return t
 
 # Define expressão regular para numeros de ponto flutuante
+
+
 def t_N_FLUTUANTE(t):
     r'[0-9]+(\.[0-9]+)(e(\+|\-)?(\d+))?'
     # t.type = "NUMERO"
@@ -78,17 +81,21 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore = ' \t'
 
-#Ignorar Comentarios
+# Ignorar Comentarios
+
+
 def t_COMMENT(t):
     r'{[^\{^\}]*}'
     pass
 
 # Erro
+
+
 def t_error(t):
-    print ("Caracter Ilegal '%s', linha %d" %(t.value[0], t.lineno))
-    print (type(t.value))
+    print("Caracter Ilegal '%s', linha %d" % (t.value[0], t.lineno))
+    print(type(t.value))
     t.lexer.skip(1)
 
 # Build the lexer
@@ -102,4 +109,4 @@ if __name__ == '__main__':
         tok = lex.token()
         if not tok:
             break
-        print (tok)
+        print(tok)
