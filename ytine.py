2962f599ddaddbb60e47c71be5cfa4303ea71f9c
Willian Moreira <willian@localhost.localdomain>
2016-05-12 11:48:59 -0300

    Refatoração de Código

--- ytine.py
+++ ytine.py
@@ -10,12 +10,6 @@ if (__name__ == "__main__"):
         code = open(sys.argv[1])
         codigo = Gen(code)
         f = open(sys.argv[1], 'r')
-        
-        # passManagerBuida = PassManagerBuilder.new()
-        # pmb.opt_level = 3
-        # pm = PassManager.new()
-        # pmb.populate(pm)
-        # pm.run(codigo.module)
 
         saida = open('build/program.ll', 'w')
         saida.write(str(codigo.module))
@@ -24,7 +18,6 @@ if (__name__ == "__main__"):
         call(["llc-3.3", "build/program.ll"])
         call(["gcc", "-c", "mylib.c", "-o", "build/mylib.o"])
         call(["gcc", "build/program.s", "build/mylib.o", "-o", sys.argv[2]])
-        # call(["chmod", "u+x", sys.argv[2]])
         print("Pronto.")
     else:
         print("Erro, parametros incorretos.\nUtilização: " + sys.argv[0] + " 'entrada' 'saida'")
